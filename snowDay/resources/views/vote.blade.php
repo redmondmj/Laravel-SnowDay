4bf7dfee2d07b4c67edf5b6d36469fe670d78a95
Matt Redmond <matt.redmond@nscc.ca>
2020-02-08 10:46:52 -0400

    added /vote

---
+++ snowDay/resources/views/vote.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row content-justify-center">
+            <div class="col-md8">
+                <button class="btn btn-success">Yes!</button>
+            </div>
+        </div>
+    </div>
+
+
+    
+@endsection
--- snowDay/resources/views/welcome.blade.php
+++ snowDay/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>NSCC Snow Day!</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,10 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Snow Day Tomorrow??
+                    Snow Day Tomorrow??
                 </div>
-
             </div>
-        </div>
     </body>
 </html>
