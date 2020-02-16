debfd5c97fe5632159a397836d37708752efc4e4
Matt Redmond <matt.redmond@nscc.ca>
2020-02-16 11:53:59 -0400

    add name and email edit

--- snowDay/app/Http/Controllers/Admin/UserController.php
+++ snowDay/app/Http/Controllers/Admin/UserController.php
@@ -62,6 +62,12 @@ class UserController extends Controller
     public function update(Request $request, User $user)
     {
         $user->roles()->sync($request->roles);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+
+        $user->save();
+
         return redirect()->route('admin.users.index');
     }
 
--- snowDay/resources/views/admin/users/edit.blade.php
+++ snowDay/resources/views/admin/users/edit.blade.php
@@ -6,24 +6,62 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                <div class="card-header">Edit User: {{ $user->name }}</div>
-                    <div class="card-body">
-                    <form action="{{ route('admin.users.update', $user) }}" method="post">
-                        {{ method_field('put') }}
-                        @csrf
-                        @foreach ($roles as $role)
-                            <div class="form-check">
-                                <input type="checkbox" name="roles[]" value="{{ $role->id }}" id="roles_{{ $role->name }}"
-                                @if ($user->hasRole($role->name))
-                                    checked
-                                @endif>
-                                <label for="roles_{{ $role->name }}">{{ $role->name }}</label>
-                            </div>
-                        @endforeach
-                        <button class="btn btn-info text-white float-left" type="submit">Update</button>
-                    </form>
+                    <div class="card-header">Edit User: {{ $user->name }}</div>
+                        <div class="card-body">
+                            <form action="{{ route('admin.users.update', $user) }}" method="post">
+                                {{ method_field('put') }}
+                                @csrf
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">Email </label>
 
-                        <a href="{{ URL::previous() }}" class="btn btn-info text-white float-left ml-5" role="button">Go Back</a>
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
+
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="name" class="col-md-4 col-form-label text-md-right">Name </label>
+
+                                    <div class="col-md-6">
+                                        <input id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user->name }}" required autocomplete="name" autofocus>
+
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="roles" class="col-md-4 col-form-label text-md-right">Roles</label>
+
+                                    <div class="col-md-6">
+                                        @foreach ($roles as $role)
+                                        <div class="form-check">
+                                            <input type="checkbox" name="roles[]" value="{{ $role->id }}" id="roles_{{ $role->name }}"
+                                            @if ($user->hasRole($role->name))
+                                                checked
+                                            @endif>
+                                            <label for="roles_{{ $role->name }}">{{ $role->name }}</label>
+                                        </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-md-4"></div>
+
+                                    <div class="col-md-6">
+                                        <button class="btn btn-info text-white float-left" type="submit">Update</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
